d0e71a439672625e5585ca401567e7b45267d45f
Eriksson Hou <hosealle@outlook.com>
2023-09-27 20:09:56 +0800

    feat: 前后端登陆请求联调成功、登陆界面修改

--- src/router/index.ts
+++ src/router/index.ts
@@ -116,10 +116,12 @@ router.beforeEach((to: toRouteType, _from, next) => {
       else document.title = item.meta.title as string;
     });
   }
+
   /** 如果已经登录并存在登录信息后不能跳转到路由白名单，而是继续保持在当前页面 */
   function toCorrectRoute() {
     whiteList.includes(to.fullPath) ? next(_from.fullPath) : next();
   }
+
   if (userInfo) {
     // 无权限跳转403页面
     if (to.meta?.roles && !isOneOfArray(to.meta?.roles, userInfo?.roles)) {
--- src/utils/http/index.ts
+++ src/utils/http/index.ts
@@ -26,7 +26,8 @@ const defaultConfig: AxiosRequestConfig = {
   // 数组格式参数序列化（https://github.com/axios/axios/issues/5142）
   paramsSerializer: {
     serialize: stringify as unknown as CustomParamsSerializer
-  }
+  },
+  baseURL: "http://localhost:8080"
 };
 
 class PureHttp {
